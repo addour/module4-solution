4a641022d5cd51afa06d318a6ffb8e4b07345de9
addour <mt.kaddour@gmail.com>
2026-01-08 15:18:49 +0100

    Fix items template: use string concatenation instead of template literals

--- src/items.component.js
+++ src/items.component.js
@@ -3,16 +3,14 @@
 
     angular.module('MenuApp')
         .component('items', {
-            template: `
-                <div>
-                    <h2>Menu Items</h2>
-                    <a ui-sref="categories" class="back-link">Back to Categories</a>
-                    <div class="menu-item" ng-repeat="item in $ctrl.items">
-                        <strong>{{item.name}}</strong><br>
-                        {{item.description}}
-                    </div>
-                </div>
-            `,
+            template: '<div>' +
+                '<h2>Menu Items</h2>' +
+                '<a ui-sref="categories" class="back-link">Back to Categories</a>' +
+                '<div class="menu-item" ng-repeat="item in $ctrl.items">' +
+                    '<strong>{{item.name}}</strong><br>' +
+                    '{{item.description}}' +
+                '</div>' +
+            '</div>',
             bindings: {
                 items: '<'
             }
