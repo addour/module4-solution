534d8bed96119c2d7bd086c3fef314653e866e2c
addour <mt.kaddour@gmail.com>
2026-01-08 15:25:36 +0100

    Update categories.component.js

--- src/categories.component.js
+++ src/categories.component.js
@@ -7,9 +7,9 @@
                 '<h2>Menu Categories</h2>' +
                 '<a ui-sref="home" class="back-link">Back to Home</a>' +
                 '<div class="category-item" ng-repeat="category in $ctrl.categories">' +
-                    '<a ui-sref="items({categoryShortName: category.short_name})">{{category.name}}</a>' +
+                '<a ui-sref="items({categoryShortName: category.short_name})">{{category.name}}</a>' +
                 '</div>' +
-            '</div>',
+                '</div>',
             bindings: {
                 categories: '<'
             }
