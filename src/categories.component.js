10980c8fc48a2b94f4e20db6439fa9a640bbc4f8
addour <mt.kaddour@gmail.com>
2026-01-08 14:53:35 +0100

    Fix categories component: remove controller, use one-way binding

--- src/categories.component.js
+++ src/categories.component.js
@@ -1,12 +1,11 @@
-categories.component.js(function() {
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('MenuApp')
-.component('categories', {
-    template: '<div><h2>Menu Categories</h2><a ui-sref="home" class="back-link">Back to Home</a><div class="category-item" ng-repeat="category in $ctrl.categories"><h3><a ui-sref="items({categoryShortName: category.short_name})">{{category.name}}</a></h3><p>{{category.special_instructions}}</p></div></div>',
-    bindings: {
-        categories: '<'
-    }
-});
-
+    angular.module('MenuApp')
+        .component('categories', {
+            template: '<div><h2>Menu Categories</h2><a ui-sref="home" class="back-link">Back to Home</a><div class="category-item" ng-repeat="category in $ctrl.categories"><a ui-sref="items({categoryShortName: category.short_name})">{{category.name}}</a></div></div>',
+            bindings: {
+                categories: '<'
+            }
+        });
 })();
