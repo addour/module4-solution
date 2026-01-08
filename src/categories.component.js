a078d00b2c0e0a26ce9e0e27057ce0ee9923c291
addour <mt.kaddour@gmail.com>
2026-01-08 15:17:39 +0100

    Fix categories template: use string concatenation instead of template literals

--- src/categories.component.js
+++ src/categories.component.js
@@ -3,15 +3,13 @@
 
     angular.module('MenuApp')
         .component('categories', {
-            template: `
-                <div>
-                    <h2>Menu Categories</h2>
-                    <a ui-sref="home" class="back-link">Back to Home</a>
-                    <div class="category-item" ng-repeat="category in $ctrl.categories">
-                        <a ui-sref="items({categoryShortName: category.short_name})">{{category.name}}</a>
-                    </div>
-                </div>
-            `,
+            template: '<div>' +
+                '<h2>Menu Categories</h2>' +
+                '<a ui-sref="home" class="back-link">Back to Home</a>' +
+                '<div class="category-item" ng-repeat="category in $ctrl.categories">' +
+                    '<a ui-sref="items({categoryShortName: category.short_name})">{{category.name}}</a>' +
+                '</div>' +
+            '</div>',
             bindings: {
                 categories: '<'
             }
