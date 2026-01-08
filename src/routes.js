0eaf6e552c5213d028fe31ca4ffabe514181db8a
addour <mt.kaddour@gmail.com>
2026-01-08 14:56:02 +0100

    Fix routes: use components without controllers, add minification protection

--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,36 @@
 (function() {
-'use strict';
+    'use strict';
 
-angular.module('MenuApp')
-.config(RoutesConfig);
+    angular.module('MenuApp')
+        .config(RoutesConfig);
 
-RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-function RoutesConfig($stateProvider, $urlRouterProvider) {
-    
-    $urlRouterProvider.otherwise('/');
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-    $stateProvider
-    .state('home', {
-        url: '/',
-        template: '<div><h2>Welcome to Our Restaurant</h2><p>Browse our delicious menu items by category.</p><a ui-sref="categories" class="back-link">View Menu Categories</a></div>'
-    })
-    .state('categories', {
-        url: '/categories',
-        template: '<categories categories="$resolve.categories"></categories>',
-        resolve: {
-            categories: ['MenuDataService', function(MenuDataService) {
-                return MenuDataService.getAllCategories();
-            }]
-        }
-    })
-    .state('items', {
-        url: '/items/{categoryShortName}',
-        template: '<items items="$resolve.items"></items>',
-        resolve: {
-            items: ['$stateParams', 'MenuDataService', function($stateParams, MenuDataService) {
-                return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
-            }]
-        }
-    });
-}
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
 
+        $stateProvider
+            .state('home', {
+                url: '/',
+                template: '<div><h1>Welcome to Our Restaurant</h1><p>Browse our delicious menu items by category.</p><a ui-sref="categories" class="back-link">View Menu Categories</a></div>'
+            })
+            .state('categories', {
+                url: '/categories',
+                component: 'categories',
+                resolve: {
+                    categories: ['MenuDataService', function(MenuDataService) {
+                        return MenuDataService.getAllCategories();
+                    }]
+                }
+            })
+            .state('items', {
+                url: '/items/{categoryShortName}',
+                component: 'items',
+                resolve: {
+                    items: ['$stateParams', 'MenuDataService', function($stateParams, MenuDataService) {
+                        return MenuDataService.getItemsForCategory($stateParams.categoryShortName);
+                    }]
+                }
+            });
+    }
 })();
