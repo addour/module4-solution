86f6e177b1ad1ae550d4e1fe024806e37fa90b18
addour <mt.kaddour@gmail.com>
2026-01-08 14:54:36 +0100

    Fix items component: remove controller, use one-way binding

--- src/items.component.js
+++ src/items.component.js
@@ -1,12 +1,11 @@
 (function() {
-'use strict';
+    'use strict';
 
-angular.module('MenuApp')
-.component('items', {
-    template: '<div><h2>Menu Items</h2><a ui-sref="categories" class="back-link">Back to Categories</a><div class="menu-item" ng-repeat="item in $ctrl.items.menu_items"><h3>{{item.name}}</h3><p><strong>Short Name:</strong> {{item.short_name}}</p><p>{{item.description}}</p></div></div>',
-    bindings: {
-        items: '<'
-    }
-});
-
+    angular.module('MenuApp')
+        .component('items', {
+            template: '<div><h2>Menu Items</h2><a ui-sref="categories" class="back-link">Back to Categories</a><div class="menu-item" ng-repeat="item in $ctrl.items"><strong>{{item.name}}</strong><br>{{item.description}}</div></div>',
+            bindings: {
+                items: '<'
+            }
+        });
 })();
